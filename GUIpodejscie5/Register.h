2260cf2117a16eb444ad520c8a6cc92daa2652ee
Kacper Orczykowski <ko310986@student.polsl.pl>
2024-06-25 01:06:11 +0200

    jednolite okna

--- GUIpodejscie5/Register.h
+++ GUIpodejscie5/Register.h
@@ -223,6 +223,7 @@ namespace GUIpodejscie5 {
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
 			this->Name = L"Register";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Register";
