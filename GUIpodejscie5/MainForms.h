1c855115493513feb109dde49486d3b27e808e47
Igor Wójcik <iw311127@student.polsl.pl>
2024-06-25 21:28:04 +0200

    obsłóga błędów

--- GUIpodejscie5/MainForms.h
+++ GUIpodejscie5/MainForms.h
@@ -47,6 +47,7 @@ namespace GUIpodejscie5 {
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label7;
 
 	private:
 		/// <summary>
@@ -71,6 +72,7 @@ namespace GUIpodejscie5 {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// label6
@@ -79,9 +81,10 @@ namespace GUIpodejscie5 {
 			this->label6->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->label6->ForeColor = System::Drawing::Color::BlueViolet;
-			this->label6->Location = System::Drawing::Point(193, 465);
+			this->label6->Location = System::Drawing::Point(257, 572);
+			this->label6->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(86, 19);
+			this->label6->Size = System::Drawing::Size(110, 23);
 			this->label6->TabIndex = 32;
 			this->label6->Text = L"Rejestracja";
 			this->label6->Click += gcnew System::EventHandler(this, &MainForms::label6_Click);
@@ -92,9 +95,10 @@ namespace GUIpodejscie5 {
 			this->label5->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->label5->ForeColor = System::Drawing::Color::Gray;
-			this->label5->Location = System::Drawing::Point(181, 446);
+			this->label5->Location = System::Drawing::Point(241, 549);
+			this->label5->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(112, 19);
+			this->label5->Size = System::Drawing::Size(140, 23);
 			this->label5->TabIndex = 31;
 			this->label5->Text = L"Nie masz konta";
 			// 
@@ -104,12 +108,14 @@ namespace GUIpodejscie5 {
 			this->checkBox2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->checkBox2->ForeColor = System::Drawing::Color::Gray;
-			this->checkBox2->Location = System::Drawing::Point(322, 321);
+			this->checkBox2->Location = System::Drawing::Point(429, 395);
+			this->checkBox2->Margin = System::Windows::Forms::Padding(4);
 			this->checkBox2->Name = L"checkBox2";
-			this->checkBox2->Size = System::Drawing::Size(92, 19);
+			this->checkBox2->Size = System::Drawing::Size(117, 23);
 			this->checkBox2->TabIndex = 30;
 			this->checkBox2->Text = L"Poka¿ has³o";
 			this->checkBox2->UseVisualStyleBackColor = true;
+			this->checkBox2->CheckedChanged += gcnew System::EventHandler(this, &MainForms::checkBox2_CheckedChanged);
 			// 
 			// button1
 			// 
@@ -117,10 +123,11 @@ namespace GUIpodejscie5 {
 			this->button1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->button1->ForeColor = System::Drawing::Color::White;
-			this->button1->Location = System::Drawing::Point(59, 361);
+			this->button1->Location = System::Drawing::Point(79, 444);
+			this->button1->Margin = System::Windows::Forms::Padding(4);
 			this->button1->Name = L"button1";
-			this->button1->Padding = System::Windows::Forms::Padding(10);
-			this->button1->Size = System::Drawing::Size(349, 70);
+			this->button1->Padding = System::Windows::Forms::Padding(13, 12, 13, 12);
+			this->button1->Size = System::Drawing::Size(465, 86);
 			this->button1->TabIndex = 29;
 			this->button1->Text = L"Zaloguj";
 			this->button1->UseVisualStyleBackColor = false;
@@ -132,9 +139,11 @@ namespace GUIpodejscie5 {
 			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
-			this->textBox2->Location = System::Drawing::Point(59, 283);
+			this->textBox2->Location = System::Drawing::Point(79, 348);
+			this->textBox2->Margin = System::Windows::Forms::Padding(4);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(349, 31);
+			this->textBox2->PasswordChar = '*';
+			this->textBox2->Size = System::Drawing::Size(465, 39);
 			this->textBox2->TabIndex = 28;
 			// 
 			// textBox1
@@ -143,9 +152,10 @@ namespace GUIpodejscie5 {
 			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
-			this->textBox1->Location = System::Drawing::Point(59, 215);
+			this->textBox1->Location = System::Drawing::Point(79, 265);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(349, 31);
+			this->textBox1->Size = System::Drawing::Size(465, 39);
 			this->textBox1->TabIndex = 27;
 			// 
 			// label2
@@ -154,9 +164,10 @@ namespace GUIpodejscie5 {
 			this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->label2->ForeColor = System::Drawing::Color::Gray;
-			this->label2->Location = System::Drawing::Point(53, 249);
+			this->label2->Location = System::Drawing::Point(71, 306);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(82, 31);
+			this->label2->Size = System::Drawing::Size(99, 38);
 			this->label2->TabIndex = 26;
 			this->label2->Text = L"Has³o";
 			// 
@@ -166,9 +177,10 @@ namespace GUIpodejscie5 {
 			this->label3->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->label3->ForeColor = System::Drawing::Color::Gray;
-			this->label3->Location = System::Drawing::Point(53, 181);
+			this->label3->Location = System::Drawing::Point(71, 223);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(82, 31);
+			this->label3->Size = System::Drawing::Size(102, 38);
 			this->label3->TabIndex = 25;
 			this->label3->Text = L"Login";
 			// 
@@ -178,28 +190,43 @@ namespace GUIpodejscie5 {
 			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->label1->ForeColor = System::Drawing::Color::BlueViolet;
-			this->label1->Location = System::Drawing::Point(105, 60);
+			this->label1->Location = System::Drawing::Point(140, 74);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(246, 55);
+			this->label1->Size = System::Drawing::Size(309, 68);
 			this->label1->TabIndex = 24;
 			this->label1->Text = L"Logowanie";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(194, 31);
+			this->label4->Location = System::Drawing::Point(259, 38);
+			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(56, 13);
+			this->label4->Size = System::Drawing::Size(68, 16);
 			this->label4->TabIndex = 33;
 			this->label4->Text = L"test udany";
 			this->label4->Visible = false;
 			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Font = (gcnew System::Drawing::Font(L"Times New Roman", 10.2F, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label7->ForeColor = System::Drawing::Color::WhiteSmoke;
+			this->label7->Location = System::Drawing::Point(75, 391);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(54, 19);
+			this->label7->TabIndex = 34;
+			this->label7->Text = L"label7";
+			// 
 			// MainForms
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::WhiteSmoke;
-			this->ClientSize = System::Drawing::Size(476, 585);
+			this->ClientSize = System::Drawing::Size(635, 720);
+			this->Controls->Add(this->label7);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label6);
 			this->Controls->Add(this->label5);
@@ -211,6 +238,7 @@ namespace GUIpodejscie5 {
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MainForms";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"MainForms";
@@ -224,14 +252,17 @@ namespace GUIpodejscie5 {
 		SqlManage db;
 		String^ password = textBox1->Text;
 		String^ login = textBox2->Text;
+
 		using namespace System::Runtime::InteropServices;
 		const char* charsl = (const char*)(Marshal::StringToHGlobalAnsi(login)).ToPointer();
 		std::string stdlogin = charsl;
 		Marshal::FreeHGlobal(IntPtr((void*)charsl));
+
 		using namespace System::Runtime::InteropServices;
 		const char* charsp = (const char*)(Marshal::StringToHGlobalAnsi(password)).ToPointer();
 		std::string stdpassword = charsp;
 		Marshal::FreeHGlobal(IntPtr((void*)charsp));
+
 		std::string sql1 = "SELECT * FROM UserData WHERE UserName='" + stdlogin + "'";
 		UserData* loginData = db.getUser(sql1.c_str());
 		if (loginData != NULL) {
@@ -242,6 +273,12 @@ namespace GUIpodejscie5 {
 		}
 		else
 		{
+			if (stdpassword != loginData->Password) {
+				label7->Text = "Zly login lub haslo";
+				label7->ForeColor = System::Drawing::Color::OrangeRed;
+			}
+			else
+				label7->ForeColor = System::Drawing::Color::OrangeRed;
 
 		}
 
@@ -251,5 +288,14 @@ namespace GUIpodejscie5 {
 		this->switchToRegister = true;
 		this->Close();
 	}
+private: System::Void checkBox2_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	if (checkBox2->Checked) {
+		textBox2->PasswordChar = NULL;
+	}
+	else {
+		textBox2->PasswordChar = '*';
+	}
+
+}
 };
 }
